25ce95d657365cd48a7c07efd87f6d8436846abc
showqeen <77331111+showqeen@users.noreply.github.com>
2021-01-27 22:31:05 +0530

    Add files via upload

---
+++ Assignment08.py
@@ -0,0 +1,51 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+
+def line_gen(A,B):
+  len =10
+  dim = A.shape[0]
+  x_AB = np.zeros((dim,len))
+  lam_1 = np.linspace(0,1,len)
+  for i in range(len):
+    temp1 = A + lam_1[i]*(B-A)
+    x_AB[:,i]= temp1.T
+  return x_AB
+
+r = -6.5 * math.sin(math.radians(20))
+z = 6.5 * math.cos(math.radians(20))
+
+#vertices
+D = np.array([0,0])
+E = np.array([6.5,0])
+F = np.array([r,z])
+
+
+#Generating all lines
+x_DE = line_gen(D,E)
+x_EF = line_gen(E,F)
+x_FD = line_gen(F,D)
+
+
+#Plotting all lines
+plt.plot(x_DE[0,:],x_DE[1,:],label='$DE$')
+plt.plot(x_EF[0,:],x_EF[1,:],label='$EF$')
+plt.plot(x_FD[0,:],x_FD[1,:],label='$FD$')
+
+
+plt.plot(D[0], D[1], 'o')
+plt.text(D[0] * (1 - 0.1), D[1] * (1) , 'D')
+plt.plot(E[0], E[1], 'o')
+plt.text(E[0] * (1 ), E[1] * (1+0.1) , 'E')
+plt.plot(F[0], F[1], 'o')
+plt.text(F[0] * (1 - 0.3), F[1] * (1 - 0.1) , 'F')
+
+
+plt.xlabel('$x$')
+plt.ylabel('$y$')
+plt.legend(loc='best')
+plt.grid()
+plt.axis('equal')
+plt.plot()
+plt.show()
